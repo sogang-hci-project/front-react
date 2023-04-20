58309044c6f2dfd1930aab44880309d7ce7c0893
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-04-20 18:14:42 +0900

    chore : 인덱스 페이지에 링크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function App() {
-	return <div></div>;
+	return (
+		<div style={{display:'flex', flexDirection:"column"}}>
+			<Link to="/interact">interact</Link>
+			<Link to="/setting">Setting</Link>
+		</div>
+	);
 }
 
 export default App;
