6f4395e7161072bd839e2900c37b970a3744b742
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-04-20 18:44:46 +0900

    feat : 텍스트, 플렉스 아톰 컴포넌트 추가

---
+++ src/components/atom/Flex/index.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import styled from "@emotion/styled";
+
+interface IProps {
+  children: ReactNode;
+  direction?: string;
+  justify?: string;
+  string?: string;
+  align?: string;
+  height?: string;
+}
+
+export function Flex({ children, direction = "column", justify = "center", align = "center", ...rest }: IProps) {
+  return (
+    <FlexBase direction={direction} justify={justify} align={align} {...rest}>
+      {children}
+    </FlexBase>
+  );
+}
+
+const FlexBase = styled("div")<{ direction?: string; justify?: string; align?: string; height?: string }>`
+  display: flex;
+  width: 100%;
+  flex-direction: ${({ direction }) => direction};
+  justify-content: ${({ justify }) => justify};
+  align-items: ${({ align }) => align};
+`;
